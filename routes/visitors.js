a1109ee0bec8585c90cf2d473995a7bab1cd2de2
javaday@gmail.com <javaday@gmail.com>
2016-11-12 12:10:07 -0700

    Updated Routes & Error Handling

--- routes/visitors.js
+++ routes/visitors.js
@@ -4,71 +4,65 @@ const db = require('../services/Database');
 exports.mountPath = '/visitors';
 exports.router = router;
 
-router.route('/:id')
-	.get(getVisitor)
-	.put(updateVisitor);
-
 router.route('/new')
-	.post(newVisitor);
+    .get(newVisitor);
+
+router.route('/:id')
+    .get(getVisitor)
+    .put(updateVisitor);
 
 router.use(defaultErrorHandler);
 
 function defaultErrorHandler(err, req, res, next) {
 
-	res.json({
-		success: false,
-		error: err
-	});
+    if (req.xhr) {
+        res.status(500).send({
+            error: err
+        });
+    } else {
+        next(err);
+    }
 }
 
 function getVisitor(req, res, next) {
 
-	let visitorId = req.params.id || 'na';
+    let visitorId = req.params.id || 'na';
 
-	db.visitors.getVisitor(visitorId)
-		.then((visitor) => {
-			res.json({
-				success: true,
-				data: visitor
-			});
-		})
-		.catch((err) => {
-			next(err);
-		});
+    db.visitors.getVisitor(visitorId)
+        .then((visitor) => {
+            res.json(visitor);
+        })
+        .catch((err) => {
+            next(err);
+        });
 }
 
 function newVisitor(req, res, next) {
-	
-	db.visitors.createVisitor()
-		.then((visitor) => {
-			res.json({
-				success: true,
-				data: visitor
-			});
-		})
-		.catch((err) => {
-			next(err);
-		});
+
+    db.visitors.createVisitor()
+        .then((visitor) => {
+            res.json(visitor);
+        })
+        .catch((err) => {
+            next(err);
+        });
 }
 
 function updateVisitor(req, res, next) {
 
-	let data = Object.assign({}, req.body);
+    let data = Object.assign({}, req.body);
 
-	for (let prop in data) {
-		if (typeof data[prop] === 'string') {
-			data[prop] = req.sanitize(data[prop]);
-		}
-	}
+    for (let prop in data) {
+        if (typeof data[prop] === 'string') {
+            data[prop] = req.sanitize(data[prop]);
+        }
+    }
 
-	db.visitors.updateVisitor(data)
-		.then((visitor) => {
-			res.json({
-				success: true,
-				data: visitor
-			});
-		})
-		.catch((err) => {
-			next(err);
-		});
+    db.visitors.updateVisitor(data)
+        .then((visitor) => {
+            res.json(visitor);
+        })
+        .catch((err) => {
+            next(err);
+        });
 }
