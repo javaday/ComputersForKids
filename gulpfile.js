40c1cba701c94f8a5a399e7e05d9a4c77504162f
Jake Overall <joverall22@gmail.com>
2016-11-16 23:58:51 -0700

    adds gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,38 @@
-const gulp = require('gulp');
-const browserify = require('browserify');
-const glob = require('glob');
-const source = require('vinyl-source-stream');
+const gulp = require('gulp')
+const browserify = require('browserify')
+const glob = require('glob')
+const source = require('vinyl-source-stream')
 
-const $ = require('gulp-load-plugins')({ lazy: true });
+const $ = require('gulp-load-plugins')({ lazy: true })
 
 gulp.task('clean', function () {
-    
-    return gulp.src('./public/bundle.js', {read: false})
-        .pipe($.clean());
-});
+  return gulp.src('./public/bundle.js', {read: false})
+    .pipe($.clean())
+})
 
-gulp.task('build', ['clean'], function() {
-    
-    log('Bundling js files into bundle.js');
-    
-	let files = glob.sync('./public/**/*.js');
+gulp.task('build', ['clean'], function () {
+  log('Bundling js files into bundle.js')
 
-    return browserify({entries: files, debug: true})
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./public/'));
-});
+  let files = glob.sync('./public/**/*.js')
+
+  return browserify({entries: files, debug: true})
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./public/'))
+})
+
+gulp.task('watch', ['build', 'clean'], function () {
+  gulp.watch('./public/**/*.js', ['build'])
+})
 
 function log (msg) {
-    
-    if(typeof(msg) === 'object') {
-        for(var item in msg) {
-            if(msg.hasOwnProperty(item)) {
-                $.util.log($.util.colors.blue(msg[item]));
-            }
-        }
+  if (typeof (msg) === 'object') {
+    for (var item in msg) {
+      if (msg.hasOwnProperty(item)) {
+        $.util.log($.util.colors.blue(msg[item]))
+      }
     }
-    else {
-        $.util.log($.util.colors.blue(msg));
-    }
+  } else {
+    $.util.log($.util.colors.blue(msg))
+  }
 }
