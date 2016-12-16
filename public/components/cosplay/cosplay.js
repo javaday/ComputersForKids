8a1beb72fd99bd333bc11d32e5418628ed01cc87
davminguez <dave.defeat@gmail.com>
2016-12-15 20:42:51 -0700

    add modal functionality

--- public/components/cosplay/cosplay.js
+++ public/components/cosplay/cosplay.js
@@ -12,9 +12,12 @@
 
     });
 
-    CosplayDetail.$inject = [];
+    app.controller('cosplayModalCtrl', CosplayModalCtrl);
 
-    function CosplayDetail() {
+    CosplayDetail.$inject = ['$uibModal', 'cosplayService'];
+    CosplayModalCtrl.$inject = ['$uibModalInstance'];
+
+    function CosplayDetail($uibModal, cosplayService) {
 
         let cd = this;
 
@@ -26,12 +29,69 @@
                 let door = document.getElementById('door-sound');
                 door.play();
                 $("#div-slide").slideToggle(2030, function () {
-
                 });
             });
-
         }
 
-    };
+        cd.signup = function (startTime) {
 
+            var modalInstance = $uibModal.open({
+                templateUrl: 'cosplayModal.html',
+                size: 'md',
+                controller: 'cosplayModalCtrl',
+                controllerAs: 'cm',
+                resolve: {}
+            });
+
+            modalInstance.result.then(
+                function(details) {
+
+                    details.startTime = startTime;
+                    cosplayService.signUp(details);
+                },
+
+                function() {
+                    //cancelled modal
+                }
+            )
+        }
+
+    }
+
+    function CosplayModalCtrl($uibModalInstance) {
+
+        let cm = this;
+
+        cm.signup = {
+            name: '',
+            email: '',
+            character: ''
+        };
+        cm.errors = [];
+
+        cm.ok = function() {
+            
+            cm.errors = [];
+
+            if(!cm.signup.name) {
+                cm.errors.push = 'Please enter your name.'
+            }
+
+            if(!cm.signup.email) {
+                cm.errors.push = 'Please enter your email.'
+            }
+
+            if(!cm.signup.character) {
+                cm.errors.push = 'Please select a character.'
+            }
+
+            if(cm.errors.length < 1) {
+                $uibModalInstance.close(cm.signup);
+            }
+        }
+        
+        cm.cancel = function () {
+            $uibModalInstance.dismiss();
+        }
+    }
 })();
--- public/services/cosplayService.js
+++ public/services/cosplayService.js
@@ -4,11 +4,33 @@
 
     app.factory('cosplayService', CosplayService);
 
-    CosplayService.$inject = ['$q', '$http', 'LocalStorageId'];
+    CosplayService.$inject = ['$q', '$http'];
 
-    function CosplayService($q, $http, LocalStorageId) {
+    function CosplayService($q, $http) {
 
+        function signUp(details) {
 
+            let deferred = $q.defer();
 
+            $http.post('/cosplay/signup', details)
+                .then((response) => {
+                    if (response.data) {
+                        deferred.resolve(response.data);
+                    }
+                    else {
+                        deferred.reject('Signup Failed');
+                    }
+                })
+                .catch((err) => {
+                    deferred.reject(err);
+                });
+
+            return deferred.promise;
+        }
+
+        return {
+            signUp: signUp
+        }
     }
+
 })();
