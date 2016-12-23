e78507ccdb9d35d6d17c2debf577deccfc5c11e5
scott-gillatt <scottgillatt@gmail.com>
2016-12-23 11:13:08 -0700

    Added NgMap to app.js and changed a little bit of naming on map

--- public/components/map/map.js
+++ public/components/map/map.js
@@ -1,21 +1,21 @@
 (function () {
 
 	let app = angular.module('cfkMayTheFourth');
-	app.component('storeLocator', {
-		controller: StoreLocator,
-		controllerAs: 'slc',
+	app.component('cfkLocation', {
+		controller: CFKLocation,
+		controllerAs: 'cl',
 		bindings: {
 		},
 		templateUrl: 'public/templates/map/map.html'
 	});
 
-	StoreLocator.$inject = ['$rootScope', 'NgMap'];
+	CFKLocation.$inject = ['$rootScope', 'NgMap'];
 
-	function StoreLocator($rootScope, NgMap) {
+	function CFKLocation($rootScope, NgMap) {
 
-		let slc = this;
+		let cl = this;
 
-		slc.markers = [{
+		cl.markers = [{
 			"name": "Computer For Kids",
 			"country": "US",
 			"coordinates": [-116.288543, 43.578843],
